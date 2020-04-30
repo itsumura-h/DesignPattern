955e6f30987eab22c88e96a6ece54c132ffc41f4
itsumura-h <dumblepy@gmail.com>
2020-04-30 21:42:10 +0900

    add uikit and original design

--- sample/frontend/basolato.lp/src/components/app.tsx
+++ sample/frontend/basolato.lp/src/components/app.tsx
@@ -6,6 +6,8 @@ import Profile from "../routes/profile";
 import NotFoundPage from '../routes/notfound';
 import Header from "./header";
 
+import 'uikit/dist/css/uikit.min.css'
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
     // tslint:disable-next-line:no-var-requires
--- sample/frontend/basolato.lp/src/components/header/index.tsx
+++ sample/frontend/basolato.lp/src/components/header/index.tsx
@@ -1,24 +1,39 @@
 import { FunctionalComponent, h } from "preact";
 import { Link } from "preact-router/match";
-import * as style from "./style.css";
+import * as s from "./style.scss";
 
 const Header: FunctionalComponent = () => {
-    return (
-        <header class={style.header}>
-            <h1>Preact App</h1>
-            <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
-                </Link>
-                <Link activeClassName={style.active} href="/profile">
-                    Me
-                </Link>
-                <Link activeClassName={style.active} href="/profile/john">
-                    John
-                </Link>
-            </nav>
-        </header>
-    );
+  return (
+    <div>
+      {/* <header class={s.header}>
+        <h1>Preact App</h1>
+        <nav>
+          <Link activeClassName={s.active} href="/">
+            Home
+              </Link>
+          <Link activeClassName={s.active} href="/profile">
+            Me
+              </Link>
+          <Link activeClassName={s.active} href="/profile/john">
+            John
+              </Link>
+        </nav>
+      </header> */}
+      <nav class="uk-navbar-container" uk-navbar style={{backgroundColor: "#000"}}>
+        <div class="uk-navbar-left">
+          <ul class="uk-navbar-nav">
+            <li><Link href="/" activeClassName={s.active}>home</Link></li>
+            <li><Link href="/profile" activeClassName={s.active}>Me</Link></li>
+            <li class="uk-parent"><Link href="/profile/john" activeClassName={s.active}>john</Link></li>
+          </ul>
+          <button class={"uk-button uk-button-default "+[s.orgButton]}>Button</button>
+          <button class="uk-button uk-button-default" disabled>Disabled</button>
+        </div>
+      </nav>
+    </div>
+
+
+  );
 };
 
 export default Header;
